3b72cec0c3d537cba3e02602287897d5415accd5
muddybeast1 <40296139+muddybeast1@users.noreply.github.com>
2024-09-21 18:53:15 +0000

    macros

--- Makefile
+++ Makefile
@@ -8,9 +8,11 @@ run: all
 
 main: $(OBJ_FILES)
 	$(GCC) $^ -o $@
+	$(some_name)
 
 %.o: %.c
 	$(GCC) $(INCLUDES) -c $^ -o $@
+	$(some_name)
 
 clean:
 	rm -f main $(OBJ_FILES)
--- includeme.mk
+++ includeme.mk
@@ -13,8 +13,16 @@ $(info SRC = $(SRC))
 
 # OBJ_FILES = $(SRC:%.c=%.o)
 OBJ_FILES = $(patsubst %.c, %.o, $(SRC))
+
 INCLUDES = -I./include/magic -I./include/magic2
 INCLUDES = $(shell find -iname "*.h" -exec dirname {} \; | sed 's/^./-I./g' | xargs)
 
 #$(info $(SRC) $(OBJ_FILES) $(INCLUDES))
 $(info This is for includeme.mk)
+
+define some_macro
+	echo "Hello World"
+	echo "Hello World! Again!" $@
+endef
+
+$(info $(some_macro))
